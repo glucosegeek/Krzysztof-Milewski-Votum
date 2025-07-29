90eba61eb0828287921cde8e0cd493218dc04e33
glucosegeek <glucosegeek@proton.me>
2025-07-29 21:00:28 +0200

    Updated StickyContactButton.tsx

--- src/components/StickyContactButton.tsx
+++ src/components/StickyContactButton.tsx
@@ -15,7 +15,7 @@ const StickyContactButton: React.FC = () => {
       showButton ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'
     }`}>
       <button 
-        className="font-bold py-2 px-4 md:py-4 md:px-8 rounded-none md:rounded-lg text-lg transition-all hover:-translate-y-2 duration-300 transform hover:scale-105 shadow-none md:shadow-lg hover:shadow-xl border-2 md:border-4 whitespace-nowrap"
+        className="font-bold py-2 px-4 md:py-4 md:px-8 rounded-lg md:rounded-lg text-lg transition-all hover:-translate-y-2 duration-300 transform hover:scale-105 shadow-none md:shadow-lg hover:shadow-xl border-2 md:border-4 whitespace-nowrap"
         style={{ backgroundColor: '#F5F5F5', borderColor: '#D4AF37', color: '#0A1A2F' }}
         onClick={handleClick}
       >
