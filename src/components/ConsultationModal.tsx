1fad4bf879571c479d6af8b646096970f7c7c3a2
glucosegeek <glucosegeek@proton.me>
2025-07-30 20:56:11 +0200

    Updated ConsultationModal.tsx

--- src/components/ConsultationModal.tsx
+++ src/components/ConsultationModal.tsx
@@ -174,17 +174,9 @@ const ConsultationModal: React.FC = () => {
           >
             Umów bezpłatną konsultację
           </button>
-          <ul>
-            <li>
               <p className="text-center text-sm mt-4" style={{ color: '#F5F5F5' }}>
             Kliknięcie przycisku przekieruje Cię do strony rezerwacji terminu w Calendly. 
-          </p></li>
-            <li>
-              <p className="text-center text-sm mt-4" style={{ color: '#F5F5F5' }}>
-            Klikając w ten przycisk zgadzasz się na politykę prywatności.
           </p>
-            </li>
-          </ul>
           
         </form>
       </div>
