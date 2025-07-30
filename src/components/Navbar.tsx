d14b66d55fae2da98eb3656e1d6020c483ee9942
glucosegeek <glucosegeek@proton.me>
2025-07-30 20:33:37 +0200

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -128,7 +128,7 @@ const Navbar: React.FC = () => {
   className="px-3 py-2 rounded-md text-sm font-medium transition-colors hover:bg-opacity-10 hover:bg-white"
   style={{ color: '#F5F5F5' }}
 >
-  Kontakt
+  O mnie
 </button>
 
           {/* Mobile Menu Button */}
