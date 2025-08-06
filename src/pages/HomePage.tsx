89eee3bb9e019d90792422e5e208dc315befe79a
glucosegeek <glucosegeek@proton.me>
2025-08-06 13:33:40 +0200

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -367,6 +367,14 @@ Nie ryzykujesz nic – możesz tylko zyskać.</li>
                 Rozpocznij swoją drogę do poprawy sytuacji finansowej od bezpłatnej konsultacji
               </p>
             </div>
+
+<button 
+              className="font-bold py-4 px-8 rounded-lg text-lg transition-all hover:-translate-y-2 duration-300 transform hover:scale-105 shadow-lg hover:shadow-xl border-4"
+              style={{ backgroundColor: '#F5F5F5', borderColor: '#D4AF37' }}
+              onClick={openModal}
+            >
+              Bezpłatna konsultacja
+            </button>
             
             <div className="grid lg:grid-cols-2 gap-12">
               <div>
