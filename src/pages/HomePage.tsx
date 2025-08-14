e6b6ec28e7bee20e2654964ea84495adb1ae8280
glucosegeek <glucosegeek@proton.me>
2025-08-14 18:17:35 +0200

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -404,7 +404,7 @@ const conciergeItems = [
       formType: 'contact_form',
       source: 'homepage_contact_section'
     };
-    const response = await fetch('https://n8n.srv948633.hstgr.cloud/webhook-test/153565ea-877e-4946-8d32-88596b5fd1d4', {
+    const response = await fetch('https://n8n.srv948633.hstgr.cloud/webhook/153565ea-877e-4946-8d32-88596b5fd1d4', {
       method: 'POST',
       headers: { 
         'Content-Type': 'application/json'
