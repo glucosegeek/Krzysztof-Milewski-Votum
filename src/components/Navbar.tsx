eace335b5a8a52df8136c38d40b5e29cb22b7212
glucosegeek <glucosegeek@proton.me>
2025-08-28 19:05:11 +0200

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -33,7 +33,7 @@ const Navbar: React.FC = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
-          <Link to="/" className="flex items-center space-x-2" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+          <Link to="/" className="h-10 w-auto sm:h-12 flex items-center space-x-2" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
            <img 
              src="/miles-logo.png" 
              alt="Krzysztof Milewski Logo" 
