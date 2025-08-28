926c994f4a78eb9d694ca44f078f9635f6b3470c
glucosegeek <glucosegeek@proton.me>
2025-08-28 19:38:50 +0200

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -37,7 +37,7 @@ const Navbar: React.FC = () => {
             <img 
               src="/miles-logo.png" 
               alt="Krzysztof Milewski Logo" 
-              className="h-22 object-contain transition-opacity hover:opacity-80"
+              className="h-21 object-contain transition-opacity hover:opacity-80"
             />
          </Link>
 
