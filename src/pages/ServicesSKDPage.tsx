d6e22629938884ab3a7a058b100dc69d32fc250f
glucosegeek <glucosegeek@proton.me>
2025-08-29 17:54:07 +0200

    Updated ServicesSKDPage.tsx

--- src/pages/ServicesSKDPage.tsx
+++ src/pages/ServicesSKDPage.tsx
@@ -18,10 +18,7 @@ const ServicesSKDPage: React.FC = () => {
     backgroundRepeat: 'no-repeat'
   }}
 >
-  {/* Ciemna nakładka */}
   <div className="absolute inset-0 bg-black/70"></div>
-  
-  {/* Treść z relative positioning */}
   <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <div className="max-w-4xl mx-auto">            
       <div className="text-center mb-16">
