a9e153a823e30ab97aa6539296e69cbf6544045e
glucosegeek <glucosegeek@proton.me>
2025-08-29 18:21:44 +0200

    Updated StickyLogo.tsx

---
+++ src/components/StickyLogo.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const StickyLogo: React.FC = () => {
+  return (
+    <div className="fixed top-4 left-4 z-50 hidden lg:block">
+      <Link 
+        to="/" 
+        className="flex items-center space-x-2" 
+        onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+      >
+        <img 
+          src="/miles-logo.png" 
+          alt="Krzysztof Milewski Logo" 
+          className="h-20 w-auto transition-opacity hover:opacity-80 drop-shadow-lg"
+        />
+      </Link>
+    </div>
+  );
+};
+
+export default StickyLogo;
