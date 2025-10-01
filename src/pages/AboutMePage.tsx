15194a23ef92457926ecb2c36364e73681cc02e0
glucosegeek <glucosegeek@proton.me>
2025-10-01 19:31:43 +0200

    Updated AboutMePage.tsx

--- src/pages/AboutMePage.tsx
+++ src/pages/AboutMePage.tsx
@@ -117,7 +117,7 @@ const AboutMePage: React.FC = () => {
       <img
         src="/aboutme-background.png"
         alt="Tło - Profesjonalne doradztwo"
-        className="w-full h-full object-cover object-center"
+        className="w-full h-auto object-cover object-center"
       />
                   
                   {/* Overlay gradient on hover */}
